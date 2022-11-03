ac1571a6198855f5e804a6ab3698cad6bcfbcf0c
quid-0 <wodnd101@naver.com>
2022-11-03 17:37:13 +0900

    modify(post) : Lazy 로딩

--- src/main/java/com/quid/sns/post/Post.java
+++ src/main/java/com/quid/sns/post/Post.java
@@ -4,6 +4,7 @@ import com.quid.sns.user.User;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,7 +35,7 @@ public class Post {
     @Column(name = "body", columnDefinition = "TEXT")
     private String body;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
