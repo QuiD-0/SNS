13011b27c555acd0475d80065250dda29ab723bd
quid-0 <wodnd101@naver.com>
2022-11-04 13:20:07 +0900

    add(comment) : 엔티티 생성

---
+++ src/main/java/com/quid/sns/comment/Comment.java
@@ -0,0 +1,50 @@
+package com.quid.sns.comment;
+
+import com.quid.sns.post.Post;
+import com.quid.sns.user.User;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @CreationTimestamp
+    private LocalDateTime registeredAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime removedAt;
+
+    @Builder
+    public Comment(User user, String content, Post post) {
+        this.user = user;
+        this.content = content;
+        this.post = post;
+    }
+}
