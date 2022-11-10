6e6ec97954fbd6172d51d1b9dbc4ef0fefe337d4
quid-0 <wodnd101@naver.com>
2022-11-10 13:51:00 +0900

    add(redis) : redis config, docker-compose 설정

---
+++ src/main/java/com/quid/sns/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.quid.sns.config;
+
+import io.lettuce.core.RedisURI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisURI redisURI = RedisURI.create(redisProperties.getUrl());
+        RedisConfiguration configuration = LettuceConnectionFactory.createRedisConfiguration(
+            redisURI);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(configuration);
+        factory.afterPropertiesSet();
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(
+        RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
