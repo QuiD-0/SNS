8918b319b27126022bf2b12f10c4a7c7bee9ceba
quid-0 <wodnd101@naver.com>
2022-11-11 17:34:23 +0900

    add(Redis) : enableCaching

--- src/main/java/com/quid/sns/SnsApplication.java
+++ src/main/java/com/quid/sns/SnsApplication.java
@@ -2,8 +2,10 @@ package com.quid.sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SnsApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/quid/sns/config/RedisConfig.java
+++ src/main/java/com/quid/sns/config/RedisConfig.java
@@ -1,16 +1,23 @@
 package com.quid.sns.config;
 
 import io.lettuce.core.RedisURI;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -39,5 +46,27 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(defaultCacheConfiguration())
+            .withInitialCacheConfigurations(customCacheConfiguration()).build();
+    }
+
+    private RedisCacheConfiguration defaultCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(1))
+            .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(
+                SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    private Map<String, RedisCacheConfiguration> customCacheConfiguration() {
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put("postList",
+            defaultCacheConfiguration().entryTtl(Duration.ofMinutes(5)));
+        return cacheConfigurations;
+    }
+
 
 }
