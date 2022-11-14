42d0113f6342b1cf3fb5c237f14bba83c18cc06f
quid-0 <wodnd101@naver.com>
2022-11-14 17:00:34 +0900

    modify(Redis) : annotataion 위치 변경

--- src/main/java/com/quid/sns/SnsApplication.java
+++ src/main/java/com/quid/sns/SnsApplication.java
@@ -2,10 +2,8 @@ package com.quid.sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class SnsApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/quid/sns/config/RedisConfig.java
+++ src/main/java/com/quid/sns/config/RedisConfig.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -15,14 +16,13 @@ import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 @RequiredArgsConstructor
-@EnableRedisRepositories
 public class RedisConfig {
 
     private final RedisProperties redisProperties;
